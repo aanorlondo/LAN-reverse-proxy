19dfe2f76c2ada114f8a7533a07d1fd3faeb4c18
aanorlondo <khaled.khebbeb@gmail.com>
2024-04-10 20:05:03 +0200

    build: rework build logic for rasp4

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -5,16 +5,13 @@ source ./prepare_env.sh
 
 # clean first
 docker rm -f LAN-PROXY-LOCAL
-docker rmi negan/lan-proxy:local
 
 # dynamically configure the nginx proxy to be in the same network than the host
-host_ip=$(ipconfig getifaddr en0)
+host_ip=$(hostname -I | awk '{print $1}')
 
 # build the proxy
-docker build -t negan/lan-proxy:local ../
-
-# push the image to remote
-docker push negan/lan-proxy:local
+docker build -t negan/lan-proxy:rasp4 ../
+docker push negan/lan-proxy:rasp4
 
 # run the proxy
 docker run \
@@ -25,4 +22,4 @@ docker run \
     -e APPDETAILS_SERVER=${APPDETAILS_SERVER} \
     --add-host="host:${host_ip}" \
     --name LAN-PROXY-LOCAL \
-    negan/lan-proxy:local
+    negan/lan-proxy:rasp4
